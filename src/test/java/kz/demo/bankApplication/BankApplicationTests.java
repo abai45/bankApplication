3206b977681ab4ea0734824f6d611d0f3bf7e75b
abaiA <abai.45.abai@gmail.com>
2024-03-31 18:18:31 +0500

    Project is Created

---
+++ src/test/java/kz/demo/bankApplication/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.demo.bankApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
